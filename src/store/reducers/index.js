64bce9b04e810a0f78e3415aae950117d423d378
Aleksandr Serov <sashaserov2010@gmail.com>
2019-03-13 21:33:32 +0300

    Connect to redux

---
+++ src/store/reducers/index.js
@@ -0,0 +1,7 @@
+import { handleActions } from "redux-actions";
+import actions from "../actions";
+import { combineReducers } from "redux";
+
+// export const items = handleActions();
+
+export default combineReducers({});
