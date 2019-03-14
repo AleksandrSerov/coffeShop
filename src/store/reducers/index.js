abeeadcaeeeb8babb40801ff0c360df81ac180bf
Aleksandr Serov <sashaserov2010@gmail.com>
2019-03-14 20:34:57 +0300

    Ready to redux

--- src/store/reducers/index.js
+++ src/store/reducers/index.js
@@ -2,6 +2,6 @@ import { handleActions } from "redux-actions";
 import actions from "../actions";
 import { combineReducers } from "redux";
 
-// export const items = handleActions();
+// export const items = handleActions([actions.initialState]);
 
 export default combineReducers({});
